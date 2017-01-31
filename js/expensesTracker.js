5f40c8e6d3592f4f05283a4a9b4f68e04de265cf
AJ <abiodunajibike@AJ.local>
2017-01-31 11:11:11 +0000

    ready for testing with karma and jasmine

--- js/expensesTracker.js
+++ js/expensesTracker.js
@@ -1,93 +1,98 @@
 //expensesTracker.js
 
-var expensesTracker = angular.module('expensesTracker', []);
+(function(){
+    'use strict';    
 
-//date formatter directive
-angular.module('expensesTracker').directive('formatDate', function(){
-    return {
-        require: 'ngModel', link: function(scope, elem, attr, modelCtrl){
-            modelCtrl.$formatters.push(function(modelValue){
-                return new Date(modelValue);
-            });
-        }
-    };
-});
-
-var api_path = '/api/expenses';
-
-expensesTracker.controller('mainController', function($scope, $http, $window, $filter){
-    scp = $scope;
-    scp.editing = false;
-    scp.formData = {};
+    var expensesTracker = angular.module('expensesTracker', []);
     
-    //Filter by amount; ascending/descending
-    scp.reOrderByAmount = function(){
-        var selected_option = scp.amount_select;
-        if (selected_option !== ''){
-            var order_field = (selected_option=== 'low_to_high') ? 'amount' : '-amount';
-            scp.expenses = $filter('orderBy')(scp.expenses, order_field);
-        }
-    };
-    
-    scp.editExpense = function(expense){
-        scp.editing = !scp.editing;
-        scp.formData = angular.copy(expense);
-    };
-    
-    //get all expenses by default
-    $http.get(api_path)
-        .success(function(data){
-            scp.expenses = data;
-            //console.log(data);
-        })
-        .error(function(err){
-            //console.log('Error: '+err);
-            alert(err);
-        });
-        
-    //add an expense
-    scp.addExpense = function(isValid){
-        
-        //console.log('isValid: '+isValid);
-        if (isValid){//check if form is valid
-            $http.post(api_path, scp.formData)
-                .success(function(data){
-                    scp.formData = {}; //clear form for new input
-                    scp.expenses = data;
-                })
-                .error(function(err){
-                    //console.log('Error: '+err);
-                    alert(err);
+    //date formatter directive
+    angular.module('expensesTracker').directive('formatDate', function(){
+        return {
+            require: 'ngModel', link: function(scope, elem, attr, modelCtrl){
+                modelCtrl.$formatters.push(function(modelValue){
+                    return new Date(modelValue);
                 });
-        }
-        
-    };
+            }
+        };
+    });
     
-    //delete an expense
-    scp.deleteExpense = function(expense_id){
-        if ($window.confirm('Are you sure you want to delete this record?')){
-            $http.delete(api_path + '/' + expense_id)
-                .success(function(data){
-                    scp.expenses = data;
-                    //console.log(data);
-                })
-                .error(function(err){
-                    //console.log('Error: '+err);
-                    alert(err);
-                });
-        }
-        
-    };
+    var api_path = '/api/expenses';
     
-    //update an expense
-    scp.updateExpense = function(expense_id){
-        //console.log('expense_id: '+expense_id);
-        $http.put(api_path + '/' + expense_id, scp.formData)
+    expensesTracker.controller('mainController', function($scope, $http, $window, $filter){
+        scp = $scope;
+        scp.editing = false;
+        scp.formData = {};
+        
+        //Filter by amount; ascending/descending
+        scp.reOrderByAmount = function(){
+            var selected_option = scp.amount_select;
+            if (selected_option !== ''){
+                var order_field = (selected_option=== 'low_to_high') ? 'amount' : '-amount';
+                scp.expenses = $filter('orderBy')(scp.expenses, order_field);
+            }
+        };
+        
+        scp.editExpense = function(expense){
+            scp.editing = !scp.editing;
+            scp.formData = angular.copy(expense);
+        };
+        
+        //get all expenses by default
+        $http.get(api_path)
             .success(function(data){
-               scp.formData = {}; //reset form
-               scp.expenses = data;
-               
-               scp.editing = !scp.editing; //toggle editing
+                scp.expenses = data;
+                //console.log(data);
+            })
+            .error(function(err){
+                //console.log('Error: '+err);
+                alert(err);
             });
-    };
-});
+            
+        //add an expense
+        scp.addExpense = function(isValid){
+            
+            //console.log('isValid: '+isValid);
+            if (isValid){//check if form is valid
+                $http.post(api_path, scp.formData)
+                    .success(function(data){
+                        scp.formData = {}; //clear form for new input
+                        scp.expenses = data;
+                    })
+                    .error(function(err){
+                        //console.log('Error: '+err);
+                        alert(err);
+                    });
+            }
+            
+        };
+        
+        //delete an expense
+        scp.deleteExpense = function(expense_id){
+            if ($window.confirm('Are you sure you want to delete this record?')){
+                $http.delete(api_path + '/' + expense_id)
+                    .success(function(data){
+                        scp.expenses = data;
+                        //console.log(data);
+                    })
+                    .error(function(err){
+                        //console.log('Error: '+err);
+                        alert(err);
+                    });
+            }
+            
+        };
+        
+        //update an expense
+        scp.updateExpense = function(expense_id){
+            //console.log('expense_id: '+expense_id);
+            $http.put(api_path + '/' + expense_id, scp.formData)
+                .success(function(data){
+                   scp.formData = {}; //reset form
+                   scp.expenses = data;
+                   
+                   scp.editing = !scp.editing; //toggle editing
+                });
+        };
+    });
+    
+})();
--- server.js
+++ server.js
@@ -4,11 +4,12 @@
 var express         = require('express'); // call express
 
 var bodyParser      = require('body-parser'); // pull information from HTML POST (express4)
-
+    morgan          = require('morgan');
+    path            = require('path');
 //var routes     = require('./routes/index');
-var expenses   = require('./js/app');
+var expenses        = require('./js/app');
 
-var mongoose = require('mongoose');   // mongoose for mongodb
+var mongoose        = require('mongoose');   // mongoose for mongodb
 
 //Database config =================================================
 mongoose.connect('mongodb://localhost/expensesTracker')
@@ -28,6 +29,15 @@ var app             = express();
 
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
 
 app.use(express.static(__dirname)); //set the static files location//
 //app.use(express.static(__dirname + '/assets')); //set the static files location
